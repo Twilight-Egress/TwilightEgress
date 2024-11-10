c7bf4f6a4abeb6b128e0644a82b0eb2d41a9c75a
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-11-10 16:23:59 -0700

    Enchanted overgrowth biome music

---
+++ Content/Biomes/EnchantedOvergrowth/EnchantedOvergrowth.cs
@@ -0,0 +1,15 @@
+﻿using Terraria.Graphics.Capture;
+
+namespace TwilightEgress.Content.Biomes.EnchantedOvergrowth
+{
+    public class EnchantedOvergrowth : ModBiome
+    {
+        public override int Music => MusicLoader.GetMusicSlot(Mod, "Assets/Sounds/Music/Overgrowth");
+
+        public override CaptureBiome.TileColorStyle TileColorStyle => CaptureBiome.TileColorStyle.Normal;
+
+        public override bool IsBiomeActive(Player player) => ModContent.GetInstance<EnchantedOvergrowthTileCount>().overgrowthTileCount >= 100;
+
+        public override SceneEffectPriority Priority => SceneEffectPriority.BiomeLow;
+    }
+}
---
+++ Content/Biomes/EnchantedOvergrowth/EnchantedOvergrowthTileCount.cs
@@ -0,0 +1,14 @@
+﻿using TwilightEgress.Content.Tiles.EnchantedOvergrowth;
+
+namespace TwilightEgress.Content.Biomes.EnchantedOvergrowth
+{
+    internal class EnchantedOvergrowthTileCount : ModSystem
+    {
+        public int overgrowthTileCount;
+
+        public override void TileCountsAvailable(ReadOnlySpan<int> tileCounts)
+        {
+            overgrowthTileCount = tileCounts[ModContent.TileType<OvergrowthDirt>()];
+        }
+    }
+}
