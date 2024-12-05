ec6e11e4e47024045fe41e05d90e3553251d7753
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-12-05 02:01:49 -0700

    cute overgrowth dirt effect
    probably optimize this later

---
+++ Core/Systems/NoiseSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwilightEgress.Core.Systems
+{
+    public class NoiseSystem : ModSystem
+    {
+        public static FastNoiseLite cellular;
+
+        public override void Load()
+        {
+            cellular = new FastNoiseLite();
+            cellular.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
+        }
+
+        public override void Unload()
+        {
+            cellular = null;
+        }
+    }
+}
