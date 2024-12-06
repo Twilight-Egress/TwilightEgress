7b24d9f6d51104fa8d95d04a2f3ec97234332dff
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-12-05 17:44:29 -0700

    wip sparkling explosive pathing

--- Content/Items/Weapons/Ranged/SparklingExplosives/SparklingGrenade.cs
+++ Content/Items/Weapons/Ranged/SparklingExplosives/SparklingGrenade.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TwilightEgress.Content.Items.Materials;
+using TwilightEgress.Content.Projectiles.Ranged.SparklingExplosives;
 
 namespace TwilightEgress.Content.Items.Weapons.Ranged.SparklingExplosives
 {
--- Content/Projectiles/Ranged/SparklingExplosives/SparklingGrenadeProjectile.cs
+++ Content/Projectiles/Ranged/SparklingExplosives/SparklingGrenadeProjectile.cs
@@ -1,8 +1,12 @@
-﻿namespace TwilightEgress.Content.Items.Weapons.Ranged.SparklingExplosives
+﻿using Terraria;
+
+namespace TwilightEgress.Content.Projectiles.Ranged.SparklingExplosives
 {
     public class SparklingGrenadeProjectile : ModProjectile
     {
         public override string Texture => "TwilightEgress/Content/Items/Weapons/Ranged/SparklingExplosives/SparklingGrenade";
+
+        private List<Vector2> mousePositions = new List<Vector2>();
 
         public override void SetStaticDefaults()
         {
@@ -43,18 +47,32 @@
 
         public override void AI()
         {
-            if (Projectile.owner == Main.myPlayer && Projectile.timeLeft <= 3)
-            {
-                Projectile.PrepareBombToBlow();
-            }
+            // Rotation increased by velocity
+            Projectile.rotation += 0.05f;
 
-            // Angle towards cursor
-            if (Projectile.timeLeft > 180)
-            {
-                float length = Projectile.velocity.Length();
-                float targetAngle = Projectile.AngleTo(Main.screenPosition + Main.MouseScreen);
-                Projectile.velocity = Projectile.velocity.ToRotation().AngleTowards(targetAngle, ToRadians(3)).ToRotationVector2() * length;
+            if (Projectile.owner != Main.myPlayer)
+                return;
+
+            if (Projectile.timeLeft <= 3)
+                Projectile.PrepareBombToBlow();
+            else {
                 Projectile.netUpdate = true;
+
+                if (mousePositions.Count == 0 || ((Main.MouseScreen + Main.screenPosition) - mousePositions.Last()).LengthSquared() >= Pow(16f * 8f, 2))
+                    mousePositions.Add(Main.MouseScreen + Main.screenPosition);
+
+                if ((mousePositions.First() - Projectile.Center).LengthSquared() < Pow(16f * 4f, 2))
+                    mousePositions.RemoveAt(0);
+
+                List<Vector2> positionsToGoTo = new List<Vector2>();
+                positionsToGoTo.AddRange(mousePositions);
+                positionsToGoTo.Add(Main.MouseScreen + Main.screenPosition);
+
+                Vector2 toFirstPos = positionsToGoTo.First() - Projectile.Center;
+                Projectile.velocity = Vector2.Lerp(Projectile.velocity, toFirstPos, 0.1f).SafeNormalize(Vector2.Zero) * 10f;
+
+                if ((positionsToGoTo.First() - Projectile.Center).LengthSquared() < Pow(16f * 1f, 2))
+                    Projectile.timeLeft = 3;
             }
 
             // Rotation increased by velocity
