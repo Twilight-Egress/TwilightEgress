a9a5d8a08c818864e278435bbaafef36820aa5d8
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-12-14 20:48:08 -0700

    run code cleanup

--- Core/Behavior/State.cs
+++ Core/Behavior/State.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TwilightEgress.Core.Behavior
+﻿namespace TwilightEgress.Core.Behavior
 {
     public abstract class State
     {
